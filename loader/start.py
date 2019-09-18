682df064ce941fac0263b2b90f1ea05ca95eae10
Patricio Napoli <pnapoli@geminis.io>
2019-09-18 02:10:15 -0300

    Added some initial code to cassandra loader.

--- loader/start.py
+++ loader/start.py
@@ -8,12 +8,22 @@ from pyspark.sql.session import SparkSession
 import json
 
 if __name__ == "__main__":
-    print("Initializing Parquet schematizer.")
+    print("Initializing Parquet loader.")
 
     fs = hdfs.hdfs(host="hadoop", port=8020, user="root")
 
     conf = SparkConf().setMaster('spark://spark-master:7077')
     sc = SparkContext(conf=conf)
     spark = SparkSession(sc)
+
+    cass_options = {"spark.cassandra.connection.host": 'cassandra', "table": "crypto", "keyspace": "enigma"}
+
+    for path in fs.list_directory("/user/root/parquet"):
+        df = spark.read.parquet(path_name)
+
+        df.write.format("org.apache.spark.sql.cassandra")
+                .mode('append')
+                .options(**cass_options)
+                .save()
 
     # TODO
